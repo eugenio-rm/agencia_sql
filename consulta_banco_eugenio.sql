725fd379b98b0ae8ab76f9bdc5ca315ae9103a2b
Eugenio Ruiz <eugenio_rm@outlook.es>
2023-04-05 13:35:16 +0200

    modificación de la consulta para provocar conflicto main

--- consulta_banco_eugenio.sql
+++ consulta_banco_eugenio.sql
@@ -1,2 +1,2 @@
 SELECT * FROM agencia_viajes.banco where sucursal like "B%" or sucursal like "C%";
-SELECT * FROM agencia_viajes.banco where sucursal like "D%" or sucursal like "E%";
+SELECT sucursal FROM agencia_viajes.banco where sucursal like "D%" or sucursal like "E%";
