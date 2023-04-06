183c35d39654ae938252d3bd3bf3405269f47d4b
Eugenio Ruiz <eugenio_rm@outlook.es>
2023-04-06 20:13:40 +0200

    modificación consultas rama main vscode

--- consulta_banco_eugenio.sql
+++ consulta_banco_eugenio.sql
@@ -1,4 +1,4 @@
 SELECT * FROM agencia_viajes.banco where sucursal like "B%" or sucursal like "C%";
-SELECT sucursal FROM agencia_viajes.banco where sucursal like "D%" or sucursal like "E%";
-SELECT nombre FROM agencia_viajes.banco where sucursal like "D%" or sucursal like "E%";
+SELECT * FROM agencia_viajes.banco where sucursal like "D%" or sucursal like "E%";
+SELECT nombre FROM agencia_viajes.banco where sucursal like "L%";
 
